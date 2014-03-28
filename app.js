746f030b61f03f62493e9bd4e7305b4f2099fc94
aereal <aereal@aereal.org>
2014-03-28 14:07:56 +0900

    Show VF image rectangle

--- app.js
+++ app.js
@@ -9,11 +9,33 @@ FinderApp.controller('FinderCalcCtrl', ['$scope', function ($scope) {
     '645'   : { width: 60.0, height: 45.0 }
   };
   $scope.sensorFormat = '135'; // default
+  $scope.vfRect = {
+    width  : '' + $scope.sensorFormatMap[$scope.sensorFormat].width * $scope.power + 'mm',
+    height : '' + $scope.sensorFormatMap[$scope.sensorFormat].height * $scope.power + 'mm'
+  };
+  $scope.sensorRect = {
+    width  : '' + $scope.sensorFormatMap[$scope.sensorFormat].width + 'mm',
+    height : '' + $scope.sensorFormatMap[$scope.sensorFormat].height + 'mm'
+  };
+  $scope.$watch('power', function (val) {
+    $scope.vfRect = {
+      width  : '' + $scope.sensorWidth * $scope.power + 'mm',
+      height : '' + $scope.sensorHeight * $scope.power + 'mm'
+    };
+  });
   $scope.$watch('sensorFormat', function (val) {
     var format = $scope.sensorFormatMap[val];
     if (format) {
       $scope.sensorWidth  = format.width;
       $scope.sensorHeight = format.height;
+      $scope.vfRect = {
+        width  : '' + $scope.sensorWidth * $scope.power + 'mm',
+        height : '' + $scope.sensorHeight * $scope.power + 'mm'
+      };
+      $scope.sensorRect = {
+        width  : '' + $scope.sensorWidth + 'mm',
+        height : '' + $scope.sensorHeight + 'mm'
+      };
     }
   });
 }]);
